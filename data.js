edfa9700259e47710d16760299ab7af9faa8a371
pawanbenjamin <pawanbenjamin@gmail.com>
2022-07-31 02:51:42 -0500

    shake options

---
+++ data.js
@@ -0,0 +1,7 @@
+export const quotes = [
+  'onus is on us.',
+  "lawbreakers can't be legislated.",
+  'an earthly loss is an ancestral gain.',
+  'a mistake is the truth trying to come out.',
+  'overturning the bullshit on a personal level by not subscribing to it is the new moevement.',
+]
--- main.js
+++ main.js
@@ -1,23 +1,12 @@
 import './style.css'
 import Shake from 'shake.js'
-
-const myShakeEvent = new Shake({
-  threshold: 15, // optional shake strength threshold
-  timeout: 1000, // optional, determines the frequency of event generation
-})
+import { quotes } from './data'
+const myShakeEvent = new Shake()
 
 myShakeEvent.start()
 
 const quoteContainer = document.querySelector('.quote')
 // const video = document.querySelector('#video')
-
-const quotes = [
-  'onus is on us.',
-  "lawbreakers can't be legislated.",
-  'an earthly loss is an ancestral gain.',
-  'a mistake is the truth trying to come out.',
-  'overturning the bullshit on a personal level by not subscribing to it is the new moevement.',
-]
 
 quoteContainer.addEventListener('click', (e) => {
   const randomIndex = Math.floor(Math.random() * quotes.length)
