e1207248a08328f25ebc8654702618981484b1d7
pawanbenjamin <pawanbenjamin@gmail.com>
2022-08-02 00:03:04 -0500

    animations

--- main.js
+++ main.js
@@ -8,16 +8,22 @@ myShakeEvent.start()
 // const video = document.querySelector('#video')
 const quoteContainer = document.querySelector('.quote')
 const moon = document.querySelector('.moon')
+const triangle = document.querySelector('.triangle')
+const clouds = document.querySelector('.clouds')
 // const video = document.querySelector('#video')
 
 quoteContainer.addEventListener('click', (e) => {
   const randomIndex = Math.floor(Math.random() * quotes.length)
   e.target.classList.toggle('hide')
   moon.classList.toggle('show-moon')
+  triangle.classList.toggle('show-triangle')
+  clouds.classList.toggle('show-clouds')
   setTimeout(() => {
     e.target.innerText = quotes[randomIndex]
     e.target.classList.toggle('hide')
     moon.classList.toggle('show-moon')
+    triangle.classList.toggle('show-triangle')
+    clouds.classList.toggle('show-clouds')
   }, 5000)
 })
 
